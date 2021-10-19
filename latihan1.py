7918428362a4f383f21bbd8074936bec61bbbdcd
ariandto <ariandto@gmail.com>
2021-10-19 13:53:13 +0700

    ver1

---
+++ latihan1.py
@@ -0,0 +1,19 @@
+##Input Data##
+name = "Budi Ariyanto."
+nickname = "Ari or Budi."
+nim ="312110159."
+born = "Kebumen"
+old = "27"
+address = "Penjalin Village at Jababeka II Cikarang."
+numb = "0895340710827 (WhatsApp Only)"
+
+##Output Data##
+print ("Assalammu'alaikum..")
+
+print ()
+
+print ("Let me introduce my self. My name is " +name,"But you can call me " +nickname,"My NIM is " +nim,"I was born in " + born,"and I am "+old,"years old.","I am very glad if you want to invite my house in " +address,"So don't forget to call me before with the number " +numb)
+
+print ()
+print ("Thank you.")
+##Output done##
